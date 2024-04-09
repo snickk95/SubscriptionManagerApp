f75b1453496d3759197ac978b362d0a56f52fb47
Andrew Stanley <astanley6786@conestogac.on.ca>
2024-04-09 14:29:33 -0400

    Added Web Client MVC App. Added CORS functionality to the web client and API.

--- SubscriptionManagerWebClient/Program.cs
+++ SubscriptionManagerWebClient/Program.cs
@@ -1,36 +1,24 @@
-using Microsoft.EntityFrameworkCore;
-using SubscriptionManagerApp.Entities;
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddRouting(options =>
-{
-    options.LowercaseUrls = true;
-    options.AppendTrailingSlash = true;
-});
-
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
 
-// Add CORS support:
 builder.Services.AddCors(options => {
     options.AddPolicy("AllowTaskClients", policy => {
         policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
     });
 });
 
-var connStr = builder.Configuration.GetConnectionString("Subscription manager");
-builder.Services.AddDbContext<SubscriptionManagerContext>(options => options.UseSqlServer(connStr));
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseCors("AllowTaskClients");
 
 app.UseHttpsRedirection();
@@ -40,6 +28,8 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();

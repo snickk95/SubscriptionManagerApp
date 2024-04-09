9d6d330bc450cc504e88be5dd4648e1be53b2748
snickk95 <51108751+snickk95@users.noreply.github.com>
2024-04-09 13:34:41 -0400

    added the connection string

--- SubscriptionManagerApp/Program.cs
+++ SubscriptionManagerApp/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
 
 var app = builder.Build();
 

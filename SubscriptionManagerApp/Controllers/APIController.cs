cc56387ce99b1234c392f56825d235565ce2aeae
snickk95 <51108751+snickk95@users.noreply.github.com>
2024-04-12 22:18:27 -0400

    added templates for routes in API controller

--- SubscriptionManagerApp/Controllers/APIController.cs
+++ SubscriptionManagerApp/Controllers/APIController.cs
@@ -1,12 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using SubscriptionManagerApp.Entities;
+using SubscriptionManagerApp.Messages;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
 
 namespace SubscriptionManagerApp.Controllers
 {
     public class APIController : Controller
     {
-        public IActionResult Index()
+        //private sub manager db context variable
+        private SubscriptionManagerContext _SubManagerDbContext;
+
+        public APIController(SubscriptionManagerContext SubManagerDbContext)
         {
-            return View();
+            _SubManagerDbContext = SubManagerDbContext;
+        }
+
+       
+        [HttpGet("/user/{email}")]
+        public async Task<IActionResult> GetUser(string email)
+        {
+          
+        }
+
+        
+        [HttpGet("/subs")]
+        public async Task<IActionResult> GetAllSubs()
+        {
+           
+        }
+
+        [HttpGet("/subs/{id}")]
+        public async Task<IActionResult> GetSubsOfUser(int Userid)
+        {
+
+        }
+
+        
+        [HttpPost("/user")]
+        public async Task<IActionResult> AddNewUser([FromBody] User UserInfo)
+        {
+
+
+        }
+
+        [HttpPost("/sub/{id}")]
+        public async Task<IActionResult> AddNewSub([FromBody] Subscription SubInfo, int UserId)
+        {
+
         }
     }
 }
